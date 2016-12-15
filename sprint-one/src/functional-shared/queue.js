b8670bda53fbbac7b5f815a59835b85e74087553
jindac <dachanman91@gmail.com>
2016-12-15 10:50:35 -0800

    Queue reports current size after dequeues.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,6 +1,7 @@
 var Queue = function() {
   var someInstance = {};
   someInstance['enqueue'] = queueMethods.enqueue;
+  someInstance['dequeue'] = queueMethods.dequeue;
   someInstance['size'] = queueMethods.size;
   someInstance['currentSize'] = 0;
   return someInstance;
@@ -9,6 +10,12 @@ var Queue = function() {
 var queueMethods = {
   enqueue: function() {
     this.currentSize++;
+  },
+  dequeue: function() {
+    if (this.currentSize !== 0) {
+      this.currentSize--;
+      
+    }
   },
   size: function() {
     return this.currentSize;
