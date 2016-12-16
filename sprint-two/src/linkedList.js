9d72e198d4e16d5e1a3372b8204bb62b60152236
jindac <dachanman91@gmail.com>
2016-12-15 21:30:53 -0800

    Implement contains function

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,47 +5,34 @@ var LinkedList = function() {
 
   list.addToTail = function(value) {
     var newNode = new Node(value);
-    if (list.tail === null) {
-      list.head = newNode;
-      list.tail = newNode;
+    if (this.tail === null) {
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-      list.tail.next = newNode;
-      list.tail = newNode;
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
   };
 
   list.removeHead = function() {
     var temp;
-    if (list.head !== null) {
-      temp = list.head.value;
-      list.head = list.head.next;
+    if (this.head !== null) {
+      temp = this.head.value;
+      this.head = this.head.next;
       return temp;
     }
   };
 
   list.contains = function(target) {
-    var start = list.head;
-    var next = start.next;
-
-    if (start.value === target) {
-      return true;
-    } else {
-      if (next === start.next) {
-        // next = start.next;
-        next.contains(target);
-      } else {
-        next = next.next;
-        if (next !== undefined) {
-          next.contains(target);
-        } else {
-          return false;
-        }
+    var currNode = this.head;
+    while (currNode !== null) {
+      if (currNode.value === target) {
+        return true;
       }
+      currNode = currNode.next;
     }
+    return false;
   };
-
-  
-
   return list;
 };
 
@@ -60,27 +47,3 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-  // list.contains = function(target) {
-  //   // Does the head.value = target?
-  //   if (this.head !== undefined) {
-  //     if (this.value === target) {
-  //       return true;
-  //     } else {
-  //       if (this.next === null) {
-  //         return false; 
-  //       } else {
-  //         this.next.contains(target);
-  //       }
-  //     }
-  //   } else {
-  //     if (this.head === target) {
-  //       return true;
-  //     } else {
-  //       if (this.head.next === null) {
-  //         return false; 
-  //       } else {
-  //         this.head.next.contains(target);
-  //       }
-  //     }
-  //   }
-  // };
