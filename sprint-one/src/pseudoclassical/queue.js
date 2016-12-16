b0d6f9c677f0250f4ab9a8d04e677cb0c3dfb393
jindac <dachanman91@gmail.com>
2016-12-15 18:52:45 -0800

    Queue allows sequentially adding and removing items.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.currentSize = 0;
+  this.storage = {};
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.currentSize++;
+  this.storage[this.currentSize] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  var temp;
+  if (this.currentSize !== 0) {
+    temp = this.storage[1];
+    for (var i = 1; i < this.currentSize; i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+    this.currentSize--;
+    return temp;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.currentSize;
 };
 
 
